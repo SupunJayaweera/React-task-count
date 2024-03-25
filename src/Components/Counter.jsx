a0c0723b099a7db481ed7674688111e6dd1b7fc4
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2024-03-25 12:27:20 +0530

    commit changes

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -18,7 +18,7 @@ class Counter extends React.Component {
     // this.setState({ count: this.state.count + 1 });
     // this.setState({ count: this.state.count + 10 });
   }
-
+  x;
   handleDefence() {
     this.setState((previousState) => {
       return {
