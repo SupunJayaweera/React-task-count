8026525248f4728aeb93f094183f444bf96cd4ec
SupunJayaweera <jayaweera_st_e22@engug.ruh.ac.lk>
2024-03-25 15:17:37 +0530

    Game functions completed

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,42 +1,110 @@
 import React from "react";
+import attack from "../images/attack.png";
+import defend from "../images/defend.png";
 
 class Counter extends React.Component {
   constructor(props) {
     super(props);
     this.handleAttack = this.handleAttack.bind(this); // binding
     this.handleDefence = this.handleDefence.bind(this);
-    this.state = { count: 0 };
+    this.state = { count: 0, gameStatus: "", lastPlay: "" };
   }
 
-  handleAttack() {
+  handleAttack = () => {
     this.setState((previousState) => {
+      let newCount = previousState.count + Math.round(Math.random() * 10);
       return {
-        count: previousState.count + 1,
+        count: newCount,
+        gameStatus: newCount > 10 ? "You Won!" : previousState.gameStatus,
+        lastPlay: "Attack",
       };
     });
 
     // this.setState({ count: this.state.count + 1 });
     // this.setState({ count: this.state.count + 10 });
-  }
+  };
   x;
-  handleDefence() {
+  handleDefence = () => {
     this.setState((previousState) => {
+      let newCount = previousState.count - Math.round(Math.random() * 10); // generating random number and round it
+
       return {
-        count: previousState.count - 1,
+        count: newCount,
+        gameStatus: newCount < -10 ? "You Lose!" : previousState.gameStatus,
+        lastPlay: "Defence",
       };
     });
-  }
+  };
+  handleRandomPlay = () => {
+    let playMode = Math.round(Math.random()); // generating random number and round it
+    if (playMode == 0) {
+      this.handleAttack();
+    } else {
+      this.handleDefence();
+    }
+  };
 
+  handleReset = () => {
+    this.setState(() => {
+      return {
+        count: 0,
+        gameStatus: "",
+        lastPlay: "",
+      };
+    });
+  };
   render() {
     return (
-      <div className="row text-white">
-        <h1>Counter: {this.state.count} </h1>
-        <button onClick={this.handleAttack} style={{ width: "200px" }}>
+      <div className="row text-white text-center">
+        <h1>Game Score : {this.state.count} </h1>
+        <p>You win at +10 points and You lose at -10 points!</p>
+        <p>Last play : {this.state.lastPlay}</p>
+        <h3>Game Status : {this.state.gameStatus}</h3>
+        <div className="col-6 col-md-3 offset-md-3">
+          <img
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid green",
+            }}
+            className="p-4 rounded"
+            src={attack}
+            onClick={this.handleAttack}
+          ></img>
+        </div>
+        <div className="col-6 col-md-3 ">
+          <img
+            style={{
+              width: "100%",
+              cursor: "pointer",
+              border: "1px solid red",
+            }}
+            className="p-4 rounded"
+            src={defend}
+            onClick={this.handleDefence}
+          ></img>
+        </div>
+        {/* <button onClick={this.handleAttack} style={{ width: "200px" }}>
           +1
-        </button>
-        <button onClick={this.handleDefence} style={{ width: "200px" }}>
+        </button> */}
+        {/* <button onClick={this.handleDefence} style={{ width: "200px" }}>
           -1
-        </button>
+        </button> */}
+        <div className="col-12 col-md-4 offset-md-4">
+          <button
+            className="btn btn-secondary w-100 mt-2"
+            onClick={this.handleRandomPlay}
+          >
+            Random Play
+          </button>
+          <br />
+          <button
+            className="btn btn-warning w-100 mt-2"
+            onClick={this.handleReset}
+          >
+            Reset
+          </button>
+        </div>
       </div>
     );
   }
--- src/Layouts/Header.jsx
+++ src/Layouts/Header.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import logo from "../images/react.png";
 
 const Header = () => {
-  const title = "CountOpedia";
+  const title = "Count Attack Game";
 
   return (
-    <div className="py-2 pl-2 " style={{ borderBottom: "1px solid #777" }}>
+    <div className="py-2 pl-2" style={{ borderBottom: "1px solid #777" }}>
       <img
         src={logo}
         alt="CountOpedia"
         style={{ width: "35px", verticalAlign: "top" }}
       />
-      <span className="h2 pt-4 text-white-50">React Course - {title}</span>
+      <span className="h2 pt-4 text-white-50">{title}</span>
     </div>
   );
 };
